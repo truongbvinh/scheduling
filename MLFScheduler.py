cabf755833fc9bd327b8ba5b7f4573a31bf76153
Vinh Truong <truongbaovinh@gmail.com>
2018-12-03 22:19:02 -0800

    Implemented Multi Level Feedback

---
+++ MLFScheduler.py
@@ -0,0 +1,52 @@
+"""
+Author Vinh Truong
+
+Implementation of Multi Level Feedback Scheduling
+preemtive scheduling algorithm
+"""
+
+from baseScheduler import baseScheduler
+from MLFProcess import MLFProcess
+
+class MLFScheduler(baseScheduler):
+    def __init__(self):
+        self.current = None
+        self.processes = [[],[],[],[],[]]
+        self.tracker = list()
+        self.current_level = None
+
+    def add_job(self, process):
+        self.processes[0].append(process)
+        self.tracker.append(process)
+    
+    def create_job(self, arrival, burst):
+        self.add_job(MLFProcess(arrival, burst, len(self.tracker)))
+    
+    def remove_job(self, process):
+        try:
+            self.processes[self.current_level].remove(process)
+        except ValueError as e:
+            print("type error but meh")
+    
+    def get_next(self):
+        if not self.isJobFinished() and self.current.time == 2 ** self.current_level:
+            self.remove_job(self.current)
+            self.current.reset_time()
+            self.processes[self._next_level(self.current_level)].append(self.current)
+            self.current, self.current_level = None, None
+
+        for level in range(5):
+            if len(self.processes[level]) > 0:
+                self.current = self.processes[level][0]
+                self.current_level = level
+                break
+        return self.current
+    
+    def _next_level(self, cur_lvl):
+        if cur_lvl < 4:
+            return cur_lvl + 1
+        return 4
+    
+    def __str__(self):
+        return "MLFScheduling"
+                
